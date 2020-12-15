c7d6b0796749c01dae08e8a8f9e2c618718327f4
RoyalPapak <54402795+RoyalPapak@users.noreply.github.com>
2020-12-15 13:48:58 +0200

    Added Logs For Dark Chat

--- index.js
+++ index.js
@@ -18,9 +18,47 @@ bot.on('ready', async () => console.log(`${bot.user.tag} is online!`))
 bot.on('message', async message => {
     if(message.author.bot) return;
   
-    if(message.channel.id === "channel_id"){ //put the dark chat channel id here
-    message.channel.send(message.content, message.attachments.first())
-    message.delete({ timeout: 2000 })
-    }
+  if(message.channel.id === "755402797549224046"){ //dark chat channelid
+message.channel.send(message.content, message.attachments.first())
+message.delete({ timeout: 3000 })
+}
+if(message.channel.id === "755402797549224046"){ //dark chat channelid
+  let channel = message.guild.channels.cache.get("744605031957004370") //dark chat logs channelid
+  if(!message.attachments.first()){ 
+    let embed = new MessageEmbed()
+    .setTitle("Dark Logs")
+    .addField("Egrapse o:", `${message.author.tag}`)
+  .addField("Mhnuma:", `${message.content}`)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setFooter("Dark Chat System Made By Papak#9999")
+  channel.send(embed)
+  }
+  
+  if(!message.content){
+    let channel2 = message.guild.channels.cache.get("744605031957004370") //dark chat logs channelid
+    let embed2 = new MessageEmbed()
+    .setTitle("Dark Logs")
+  .addField("Egrapse o:", `${message.author.tag}`)
+  .addField("Mhnuma:", `Den egrapse tipota.Apla esteile eikona.`)
+  .setImage(message.attachments.first().proxyURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setFooter("Dark Chat System Made By Papak#9999")
+    channel2.send(embed2)
+  }
+  else if(message.content, message.attachments.first()){
+    let channel3 = message.guild.channels.cache.get("744605031957004370") //dark chat logs channelid
+    let embed3 = new MessageEmbed()
+    .setTitle("Dark Logs")
+  .addField("Egrapse o:", `${message.author.tag}`)
+  .addField("Mhnuma:", `${message.content}`)
+  .setImage(message.attachments.first().proxyURL)
+  .setColor("RANDOM")
+  .setTimestamp()
+  .setFooter("Dark Chat System Made By Papak#9999")
+    channel3.send(embed3)
+  }
+} 
     });
 bot.login("token here") // you will find it at (https://discord.com/developers/applications)
